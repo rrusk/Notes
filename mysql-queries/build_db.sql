4bf457c00ca2be7b2b686d1aaff67b647ffbdce4
Raymond Rusk <rusk.raymond@gmail.com>
2015-10-26 11:27:39 -0700

    Work in progress

---
+++ mysql-queries/build_db.sql
@@ -0,0 +1,25 @@
+-- Data for McMaster Hospital inserted into clinic
+-- table by oscardata.sql during database creation.
+SELECT 'demographics_insert.sql' AS '';
+source demographics_insert.sql
+SELECT 'drugs_insert.sql' AS '';
+source drugs_insert.sql
+SELECT 'dxresearch_insert.sql' AS '';
+source dxresearch_insert.sql
+SELECT 'allergies_insert.sql' AS '';
+source allergies_insert.sql
+SELECT 'preventions_insert.sql' AS '';
+source preventions_insert.sql
+SELECT 'casemgmt_issue_insert.sql' AS '';
+source casemgmt_issue_insert.sql
+SELECT 'casemgmt_note_insert.sql' AS '';
+source casemgmt_note_insert.sql
+SELECT 'measurements_insert.sql' AS '';
+source measurements_insert.sql
+-- provider_insert.sql
+-- casemgmt_issue_notes_insert.sql
+-- casemgmt_note_ext_insert.sql
+-- hl7TextInfo_insert.sql
+-- measurementsExt_insert.sql
+-- patientLabRouting_insert.sql
+-- preventionsExt_insert.sql
