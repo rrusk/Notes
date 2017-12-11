fda5ba6de9ae18923203a5b5400ca224bdba59c5
Raymond Rusk <Raymond.Rusk@shaw.ca>
2017-12-11 14:56:56 -0800

    Re-initializing vagrant box

---
+++ VMs/cleanup-kernels.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e # exit on errors
+dpkg --list | grep 'linux-image' | awk '{ print $2 }' | sort -V | sed -n '/'"$(uname -r | sed "s/\([0-9.-]*\)-\([^0-9]\+\)/\1/")"'/q;p' | xargs sudo apt-get -y purge
+dpkg --list | grep 'linux-headers' | awk '{ print $2 }' | sort -V | sed -n '/'"$(uname -r | sed "s/\([0-9.-]*\)-\([^0-9]\+\)/\1/")"'/q;p' | xargs sudo apt-get -y purge
+# probably not necessary but do anyway
+sudo update-grub2
