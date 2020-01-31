fec3514421bef7ec7a3732dccfb18077069cab8d
Raymond Rusk <Raymond.Rusk@shaw.ca>
2020-01-30 17:57:12 -0800

    Script to fix NTFS filenames

---
+++ fix_ntfs_filenames/README.txt
@@ -0,0 +1,1 @@
+The purpose of this script is to fix filenames so they can be copied to an NTFS drive.
---
+++ fix_ntfs_filenames/fix_ntfs_filenames.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+#
+
+import string
+import os
+import re
+import sys
+
+if len(sys.argv) != 2:
+    print()
+    print("Usage:", sys.argv[0], "<directory>")
+    print("  where <directory> is the folder you want to check for filenames")
+    print("    and directory names that are not valid in an NTFS filesystem.")
+    print("  Will check all files and directories in <directory> and its subdirectories.")
+    print()
+    exit()
+
+# From https://stackoverflow.com/questions/6745097/how-to-check-for-platform-incompatible-folder-file-names-in-python
+ILLEGAL_NTFS_CHARS = "[<>:/\\|?*\"]|[\0-\31]"
+
+
+def __removeIllegalChars(name):
+    # removes characters that are invalid for NTFS
+    return re.sub(ILLEGAL_NTFS_CHARS, "_", name)
+
+
+# Set the path you want to check
+path = sys.argv[1]
+
+# Check directory names for illegal characters
+for root, dirs, files in os.walk(path):
+    for dirname in dirs:
+        fixdirname = __removeIllegalChars(dirname)
+        if fixdirname != dirname:
+            olddirname = os.path.join(root, dirname)
+            newdirname = os.path.join(root, fixdirname)
+            print("Rename directory [" + olddirname + "]")
+            print("    to directory [" + newdirname + "]")
+            getYN = input("<Y/N>: ").strip()
+            if getYN == "Y" or getYN == "y":
+                if not os.path.exists(newdirname):
+                    os.rename(olddirname, newdirname)
+                else:
+                    print(
+                        "Sorry, that conflicts with a directory or file name that aleady exists")
+
+# Check each file in subfolders
+for root, dirs, files in os.walk(path):
+    for name in files:
+        fixname = __removeIllegalChars(name)
+        if name != fixname:
+            oldfilename = os.path.join(root, name)
+            newfilename = os.path.join(root, fixname)
+            print("Rename file [" + oldfilename + "]")
+            print("    to file [" + newfilename + "]")
+            getYN = input("<Y/N>: ").strip()
+            if getYN == "Y" or getYN == "y":
+                if not os.path.exists(newfilename):
+                    os.rename(oldfilename, newfilename)
+                else:
+                    print(
+                        "Sorry, that conflicts with a file or directory name that aleady exists")
